79bdfbde6ad18dbe4b38a3f8bd28664f37141efa
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-31 12:36:55 +0400

    added doc comment

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -1,3 +1,5 @@
 package consts
 
+// GontainerHelperPath is an import path to gontainer-helpers modules.
+// Updating a path here should update it in all used places in printed container.
 const GontainerHelperPath = "github.com/gomponents/gontainer-helpers"
