6a85a54a33ba0fc7364b685655153131a7e07157
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-31 19:41:48 +0400

    unit tests

--- pkg/dto/input/dto_test.go
+++ pkg/dto/input/dto_test.go
@@ -9,24 +9,66 @@ import (
 )
 
 func TestCall_UnmarshalYAML(t *testing.T) {
-	// todo more tests
-	y := `
-- ["SetName", ["Mary"], true]
-`
-	calls := make([]Call, 0)
-	err := yaml.Unmarshal([]byte(y), &calls)
-	assert.NoError(t, err)
-	assert.Equal(
-		t,
-		[]Call{
-			{
+	scenarios := []struct {
+		input  string
+		output Call
+		error  string
+	}{
+		{
+			input: `
+- SetName
+- ["Mary"]
+- true`,
+			output: Call{
 				Method:    "SetName",
 				Args:      []interface{}{"Mary"},
 				Immutable: true,
 			},
 		},
-		calls,
-	)
+		{
+			input: `[SetName, ["Mary"], false]`,
+			output: Call{
+				Method:    "SetName",
+				Args:      []interface{}{"Mary"},
+				Immutable: false,
+			},
+		},
+		{
+			input: `
+- SetName
+- ["Mary"]
+- 115`,
+			error: "third element of object Call must be a bool, `int` given",
+		},
+		{
+			input: `
+- SetName
+- false
+- true`,
+			error: "second element of object Call must be an array, `bool` given",
+		},
+		{
+			input: `[78, ["Mary"], false]`,
+			error: "first element of object Call must be a string, `int` given",
+		},
+		{
+			input: `[1, 2, 3, 4]`,
+			error: "object Call must contain 1 - 3 args, 4 given",
+		},
+	}
+
+	for i, s := range scenarios {
+		t.Run(fmt.Sprintf("Scenario #%d", i), func(t *testing.T) {
+			call := Call{}
+			err := yaml.Unmarshal([]byte(s.input), &call)
+			if s.error != "" {
+				assert.EqualError(t, err, s.error)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, s.output, call)
+		})
+	}
 }
 
 func TestCreateDefaultDTO(t *testing.T) {
