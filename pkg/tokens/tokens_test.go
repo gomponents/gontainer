162e87da7db34b186b4a17c0741dc68a88ffd64d
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-31 13:59:18 +0400

    unit tests

--- pkg/tokens/tokens_test.go
+++ pkg/tokens/tokens_test.go
@@ -26,3 +26,26 @@ func Test_toExpr(t *testing.T) {
 		})
 	}
 }
+
+func TestTokenPercentSign_Supports(t *testing.T) {
+	t.Run("Given supported scenario", func(t *testing.T) {
+		assert.True(t, TokenPercentSign{}.Supports("%%"))
+	})
+	t.Run("Given unsupported scenario", func(t *testing.T) {
+		assert.False(t, TokenPercentSign{}.Supports("hello"))
+	})
+}
+
+func TestTokenPercentSign_Create(t *testing.T) {
+	tkn, err := TokenPercentSign{}.Create("%%")
+	assert.NoError(t, err)
+	assert.Equal(
+		t,
+		Token{
+			Kind: KindCode,
+			Raw:  "%%",
+			Code: `"%"`,
+		},
+		tkn,
+	)
+}
