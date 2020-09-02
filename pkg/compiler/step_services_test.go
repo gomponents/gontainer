ca75aa7ece0582dbf633db621f9125d6e173acd1
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-09-02 22:16:46 +0400

    tests

--- pkg/compiler/step_services_test.go
+++ pkg/compiler/step_services_test.go
@@ -1,7 +1,12 @@
 package compiler
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/gomponents/gontainer/pkg/dto/compiled"
+	"github.com/gomponents/gontainer/pkg/dto/input"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestStepServices_handleServiceType(t *testing.T) {
@@ -101,6 +106,42 @@ func TestStepServices_handleServiceConstructor(t *testing.T) {
 		}.handleServiceConstructor,
 		scenarios...,
 	)
+}
+
+func TestStepServices_Do(t *testing.T) {
+	t.Run("Sort services", func(t *testing.T) {
+		scenarios := []struct {
+			input  map[string]input.Service
+			output []compiled.Service
+		}{
+			{
+				input: map[string]input.Service{
+					"logger":     {Todo: true},
+					"db":         {Todo: true},
+					"httpClient": {Todo: true},
+				},
+				output: []compiled.Service{
+					{Name: "db", Todo: true},
+					{Name: "httpClient", Todo: true},
+					{Name: "logger", Todo: true},
+				},
+			},
+			{
+				input:  nil,
+				output: nil,
+			},
+		}
+
+		for i, s := range scenarios {
+			t.Run(fmt.Sprintf("Scenario #%d", i), func(t *testing.T) {
+				i := input.DTO{Services: s.input}
+				o := compiled.DTO{}
+				err := StepServices{}.Do(&i, &o)
+				assert.NoError(t, err)
+				assert.Equal(t, s.output, o.Services)
+			})
+		}
+	})
 }
 
 type mockImports struct {
