d889eae80c3a40eb1484f77a06513ba3d75772ef
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-09-02 19:35:30 +0400

    rewriting compiler

---
+++ pkg/compiler/step_params.go
@@ -0,0 +1,48 @@
+package compiler
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/gomponents/gontainer/pkg/dto/compiled"
+	"github.com/gomponents/gontainer/pkg/dto/input"
+	"github.com/gomponents/gontainer/pkg/parameters"
+)
+
+type StepParams struct {
+	paramResolver parameters.Resolver
+}
+
+func NewStepParams(paramResolver parameters.Resolver) *StepParams {
+	return &StepParams{paramResolver: paramResolver}
+}
+
+func (s StepParams) Do(i *input.DTO, r *compiled.DTO) error {
+	var names []string
+	for n, _ := range i.Params { //nolint:gosimple
+		names = append(names, n)
+	}
+	sort.Strings(names)
+	for _, n := range names {
+		v := i.Params[n]
+		param, err := s.paramResolver.Resolve(v)
+		if err != nil {
+			return fmt.Errorf("cannot resolve param `%s`", n)
+		}
+		r.Params = append(
+			r.Params,
+			compiled.Param{
+				Name:      n,
+				Code:      param.Code,
+				Raw:       param.Raw,
+				DependsOn: param.DependsOn,
+			},
+		)
+	}
+
+	sort.SliceStable(r.Params, func(i, j int) bool {
+		return r.Params[i].Name < r.Params[j].Name
+	})
+
+	return nil
+}
