a5065bcf9997f4ae8f49e9381f9b19c07f7d03bd
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-09-02 21:25:13 +0400

    tests

--- pkg/compiler/step_services_test.go
+++ pkg/compiler/step_services_test.go
@@ -39,3 +39,52 @@ func (m mockImports) GetAlias(string) string {
 func (m mockImports) RegisterPrefix(shortcut string, path string) error {
 	return m.error
 }
+
+func TestStepServices_handleServiceValue(t *testing.T) {
+	scenarios := []inputOutputScenario{
+		{
+			input:  "MyValue",
+			output: "MyValue",
+		},
+		{
+			input:  "my/import.MyValue",
+			output: "alias.MyValue",
+		},
+		{
+			input:  `"my/import".MyValue`,
+			output: "alias.MyValue",
+		},
+		{
+			input:  `"my/import".MyStruct{}.MyMethod`,
+			output: "alias.MyStruct{}.MyMethod",
+		},
+		{
+			input:  `&"my/import".MyStruct{}`,
+			output: "&alias.MyStruct{}",
+		},
+		{
+			input:  `&my/import.MyStruct{}`,
+			output: "&alias.MyStruct{}",
+		},
+		{
+			input:  `&MyStruct{}`,
+			output: "&MyStruct{}",
+		},
+		{
+			input:  `MyStruct{}`,
+			output: "MyStruct{}",
+		},
+		{
+			input:  "",
+			output: "",
+		},
+	}
+
+	doTestInputOutput(
+		t,
+		StepServices{
+			aliases: mockImports{alias: "alias"},
+		}.handleServiceValue,
+		scenarios...,
+	)
+}
