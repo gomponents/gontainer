b843a54e7b7fab4221d988126c585356f17960ca
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-09-04 18:25:49 +0400

    added example of !value

--- examples/global-var/main.go
+++ examples/global-var/main.go
@@ -9,10 +9,25 @@ var (
 	runtimeTime time.Time
 )
 
+type mysqlTime struct {
+	*time.Time
+}
+
+func newMysqlTime(time *time.Time) *mysqlTime {
+	return &mysqlTime{Time: time}
+}
+
+func (m mysqlTime) MysqlString() string {
+	return m.In(time.UTC).Format("2006-01-02 15:04:05")
+}
+
 func main() {
 	c := NewContainer()
 	t, _ := c.GetTime()
 	fmt.Printf("Time: %s\n", t.Format(time.RFC3339))
+
+	m, _ := c.GetMysqlTime()
+	fmt.Println(m.MysqlString())
 }
 
 func init() {
